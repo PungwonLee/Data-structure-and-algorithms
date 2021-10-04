ff0bc1952900b16ebdeb67909dcf1f9bc70d5242
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2021-10-04 09:59:50 +0900

    Update al_MergeSort.c

--- al_MergeSort.c
+++ al_MergeSort.c
@@ -1,65 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include <stdbool.h>    // bool, true, false가 정의된 헤더 파일
 #include<stdlib.h>
 #include<math.h>
-int* MergeFunc(int* leftArr, int* rigthArr, int leftSize, int rigthSize) {
-	int* mergeArr = (int*)malloc(sizeof(int) * (leftSize + rigthSize));
-	int leftidx = 0;
-	int rigthidx = 0;
-	int mergeidx = 0;
-	while (leftSize > leftidx && rigthSize > rigthidx) {
-		if (leftArr[leftidx] > rigthArr[rigthidx]) {
-			mergeArr[mergeidx++] = rigthArr[rigthidx];
-			rigthidx++;
-		}
-		else {
-			mergeArr[mergeidx++] = leftArr[leftidx];
-			leftidx++;
-		}
-	}
-	while (leftSize > leftidx)
-		mergeArr[mergeidx++] = leftArr[leftidx++];
+#include<string.h>
 
-	while (rigthSize > rigthidx)
-		mergeArr[mergeidx++] = rigthArr[rigthidx++];
-
-	return mergeArr;
-}
-
-int *SplitFunc(int* arr, int size) {
-	if (size <= 1) return arr;
-
-	int mid = size / 2;
-	int* leftArr = (int*)malloc(sizeof(int) * mid);
-	int* rightArr = (int*)malloc(sizeof(int) * (size - mid));
-	
-	for (int i = 0; i < mid; i++)
-		leftArr[i] = arr[i];
-
-	leftArr=SplitFunc(leftArr, mid);
-
-	for (int i = 0; i < size-mid; i++)
-		rightArr[i] = arr[i + mid];
-
-	rightArr=SplitFunc(rightArr, size-mid);
-
-	return MergeFunc(leftArr, rightArr, mid, size - mid);
-}
-
-
+void inPlaceQuickSort(int* L, int l, int r);
+int findPivot(int* L, int l, int r);
+int inPlacePartition(int* L, int l, int r, int k);
 int main() {
 	int n;
 	scanf("%d", &n);
-	int* arr = (int*)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &arr[i]);
+	int* arr = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
+	inPlaceQuickSort(arr, 0, n - 1);
+
+	for (int i = 0; i < n; i++)printf("%d ", arr[i]);
+
+	free(arr);
+}
+
+void inPlaceQuickSort(int* L, int l, int r) {
+	if (l >= r)
+		return;
+	int k = findPivot(L, l, r);
+	int i=inPlacePartition(L, l, r, k);
+	inPlaceQuickSort(L, l, i - 1);
+	inPlaceQuickSort(L, i + 1, r);
+
+}
+int findPivot(int* L, int l, int r) {
+	return rand() % (r - l+1) + l;
+}
+int inPlacePartition(int* L, int l, int r, int k) {
+	int p = L[k];
+	int tmp = L[k];
+	L[k] = L[r];
+	L[r] = tmp;
+
+	int i = l;
+	int j = r - 1;
 	
-
-	arr=SplitFunc(arr, n);
-
-	for (int i = 0; i < n; i++)
-		printf("%d\n", arr[i]);
-	printf("\n");
+	while (i <= j) {
+		while (i <= j && L[i] <= p)
+			i++;
+		while (j >= i && L[j] >= p)
+			j--;
+		if (i < j) {
+			tmp = L[i];
+			L[i] = L[j];
+			L[j] = tmp;
+		}		
+	}
+	 tmp = L[i];
+	L[i] = L[r];
+	L[r] = tmp;
+	return i;
 }
