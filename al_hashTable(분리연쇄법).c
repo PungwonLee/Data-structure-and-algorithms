f13f86152f28971d8b21951e6f67e0e74db4eaaa
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2021-12-11 03:02:33 +0900

    Update al_hashTable(분리연쇄법).c

--- al_hashTable(분리연쇄법).c
+++ al_hashTable(분리연쇄법).c
@@ -3,22 +3,22 @@
 #include<stdlib.h>
 
 typedef struct Node {
-	struct Node * next;
+	struct Node* next;
 	int data;
 }Node;
 
-int hash(int data,int m);
-void insert(Node* v, int data,int m);
-int search(Node* v, int data,int m);
-int deletetable(Node* v, int data,int m);
-void printtable(Node* v,int m);
+int hash(int data, int m);
+void insert(Node* v, int data, int m);
+int search(Node* v, int data, int m);
+int deletetable(Node* v, int data, int m);
+void printtable(Node* v, int m);
 Node* initNode() {
 	Node* node = (Node*)malloc(sizeof(Node));
 	node->next = NULL;
 	return node;
 }
 int main() {
-	int m,data;
+	int m, data;
 	char op;
 	scanf("%d", &m);
 	Node* arr = (Node*)malloc(m * sizeof(Node));
@@ -29,42 +29,44 @@ int main() {
 
 		if (op == 'i') {
 			scanf(" %d", &data);
-			insert(arr, data,m);
+			insert(arr, data, m);
 		}
 		else if (op == 's') {
 			scanf(" %d", &data);
-			printf("%d\n",search(arr, data,m));
+			printf("%d\n", search(arr, data, m));
 		}
 		else if (op == 'd') {
 			scanf(" %d", &data);
 			printf("%d\n", deletetable(arr, data, m));
-
 		}
-		else if (op == 'p') {
+		else if (op == 'p')
 			printtable(arr, m);
-		}
 		else break;
 	}
-
+	for (int i = 0; i < m; i++) {
+		Node* p = arr[i].next;
+		while (p != NULL) {
+			Node* tmp = p;
+			p = p->next;
+			free(tmp);
+		}
+	}
+	free(arr);
 }
-int hash(int data,int m) {
+int hash(int data, int m) {
 	return data % m;
 }
-void insert(Node* v, int data,int m) {
+void insert(Node* v, int data, int m) {
 	Node* node = initNode();
 	node->data = data;
-
 	node->next = v[hash(data, m)].next;
-	v[hash(data, m)].next=node;
-	
-	
-	
+	v[hash(data, m)].next = node;
 
 }
-int search(Node* v, int data,int m) {
+int search(Node* v, int data, int m) {
 	int idx = 0;
 	Node* p = v[hash(data, m)].next;
-	while (p != NULL ) {
+	while (p != NULL) {
 		idx++;
 		if (p->data == data)return idx;
 		p = p->next;
@@ -72,17 +74,18 @@ int search(Node* v, int data,int m) {
 	return 0;
 }
 int deletetable(Node* v, int data, int m) {
-	
+
 	Node* p = v[hash(data, m)].next;
 	if (p == NULL) return 0;
+
 	if (p->data == data) {
-		v[hash(data, m)].next = v[hash(data,m)].next->next;
+		v[hash(data, m)].next = v[hash(data, m)].next->next;
 		free(p);
 		return 1;
 	}
-	
+
 	int idx = 1;
-	
+
 	while (p->next != NULL) {
 		idx++;
 		if (p->next->data == data) {
@@ -93,11 +96,10 @@ int deletetable(Node* v, int data, int m) {
 		}
 		p = p->next;
 	}
-	
+
 	return 0;
 }
-void printtable(Node* v,int m) {
-	
+void printtable(Node* v, int m) {
 	for (int i = 0; i < m; i++) {
 		Node* p = v[i].next;
 		while (p != NULL) {
